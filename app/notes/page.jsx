c7fd62ab6d83357e70c965660039278a00871f44
aly8i <ABY00003@students.aust.edu.lb>
2023-10-11 17:24:30 +0300

    added notes

---
+++ app/notes/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Notes from "../../components/Notes/Notes";
+const Page = () => {
+  return (
+    <Notes/>
+  )
+};
+
+export default Page;
---
+++ components/Notes/Notes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./Notes.module.scss"
+const Notes = () => {
+  return (
+    <div className={styles.wrapper}>
+        <a href='https://lebanontoken.vercel.app'>Lebanon Token</a>
+        <a href='https://ali-bilal-yaghis-team.adalo.com/rashresh?_gl=1%2A1b1nbuo%2A_ga%2AMTU2Mzk3Mjg1OC4xNjk3MDMzOTkx%2A_ga_SWT45DV35L%2AMTY5NzAzMzk5MC4xLjAuMTY5NzAzMzk5MC42MC4wLjA.&target=8qvnudfaqkdezzcud1fr43lft&params=%7B%7D'>Dekene w Bharat</a>
+    </div>
+  )
+}
+
+export default Notes
